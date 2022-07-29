054086e1c51e607439bfd98501f83570a87cc168
Rennll <dt616717@gmail.com>
2022-07-29 14:52:42 +0800

    fix: pass the user like and tweet like test

--- controllers/like-controller.js
+++ controllers/like-controller.js
@@ -2,33 +2,33 @@ const { Tweet, Like } = require('../models')
 const helpers = require('../_helpers')
 
 const likeController = {
-    getUserLike: (req, res, next) => {
-        const userId = Number(req.params.id)
+  getUserLike: (req, res, next) => {
+    const userId = Number(req.params.id)
 
-        return Tweet.findAll({ where: { userId }, raw: true, nest: true })
-            .then(tweets => {
-                if (!tweets.length) throw new Error("There isn't tweet that user liked.")
+    return Tweet.findAll({ where: { userId }, raw: true, nest: true })
+      .then(tweets => {
+        if (!tweets.length) throw new Error("There isn't tweet that user liked.")
 
-                res.json({ status: 'success', ...tweets })
-            })
-            .catch(err => next(err))
-    },
-    postLike: (req, res, next) => {
-        const tweetId = Number(req.params.id)
-        const userId = Number(helpers.getUser(req))
+        res.json({ status: 'success', ...tweets })
+      })
+      .catch(err => next(err))
+  },
+  postLike: (req, res, next) => {
+    const tweetId = Number(req.params.id)
+    const userId = Number(helpers.getUser(req).id)
 
-        return Tweet.findByPk(tweetId)
-            .then(tweet => {
-                if (!tweet) throw new Error("This tweet isn't Existed.")
+    return Tweet.findByPk(tweetId)
+      .then(tweet => {
+        if (!tweet) throw new Error("This tweet isn't Existed.")
 
-                return Like.create({
-                    UserId: userId,
-                    TweetId: tweetId
-                })
-            })
-            .then(() => res.redirect('back'))
-            .catch(err => next(err))
-    }
+        return Like.create({
+          UserId: userId,
+          TweetId: tweetId
+        })
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  }
 }
 
 module.exports = likeController
