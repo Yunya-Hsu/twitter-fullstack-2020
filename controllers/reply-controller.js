2d3aa783af644f77dbc54c3bc8949a51f4c53c64
忍者哈特利 <ks0dcongra@gmail.com>
2022-08-01 02:47:05 +0800

    feat: succes to add reply and like feature

--- controllers/reply-controller.js
+++ controllers/reply-controller.js
@@ -1,4 +1,4 @@
-const { User, Reply, Tweet } = require('../models')
+const { User, Reply, Tweet, Like } = require('../models')
 const helpers = require('../_helpers')
 const handlebars = require('handlebars')
 handlebars.registerHelper('dateFormat', require('handlebars-dateformat'))
@@ -7,17 +7,33 @@ const replyController = {
   getReply: (req, res, next) => {
     Promise.all([
       Tweet.findByPk(req.params.tid, {
+        order: [
+          [Reply, 'createdAt', 'desc']
+        ],
         include: [
           { model: Reply, include: User },
-          { model: User }
+          { model: User },
+          { model: Like }
         ]
       }),
       User.findAll({
-        include: [{ model: User, as: 'Followers' }]
+        where: { role: 'user' },
+        attributes: ['id', 'avatar', 'name', 'account'],
+        include: [{
+          model: User,
+          attributes: ['id'],
+          as: 'Followers'
+        }],
+        nest: true
       })
     ])
       .then(([tweet, users]) => {
         if (!tweet) throw new Error("tweet didn't exist!")
+        tweet = JSON.parse(JSON.stringify(tweet))
+        const userInfo = tweet.User
+        const likesNum = tweet.Likes.length
+        const likedTweetsId = helpers.getUser(req)?.LikeTweets ? helpers.getUser(req).LikeTweets.map(lt => lt.id) : []
+        userInfo.isLiked = likedTweetsId.includes(tweet.id) ? tweet.isLiked = likedTweetsId.includes(tweet.id) : false
         users = users.map(user => ({
           ...user.toJSON()
         }))
@@ -28,20 +44,18 @@ const replyController = {
           }
         })
         users = filterSelfAndAdminUser.sort((a, b) => b.followerCount - a.followerCount).slice(0, 10)
-        res.render('reply', { tweet: tweet.toJSON() })
+        res.render('reply', { tweet, userInfo, likesNum })
       })
       .catch(err => next(err))
   },
   postReply: (req, res, next) => {
     const tweetId = Number(req.params.tid)
     const userId = Number(helpers.getUser(req).id)
-    const comment = req.body.tweet
-    if (!comment) throw new Error('Need to enter words.')
-
+    const comment = req.body.comment
+    if (!comment) throw new Error('需要輸入文字.')
     Tweet.findByPk(tweetId)
       .then(tweet => {
-        if (!tweet) throw new Error("The tweet isn't existed.")
-
+        if (!tweet) throw new Error('這則推文不存在!')
         return Reply.create({
           comment,
           TweetId: tweetId,
