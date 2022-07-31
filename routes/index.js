eca5b7ac972c3d6180fd0262d0c076e1e5dba37e
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-31 19:32:24 +0800

    feat: add replies modal and like modal

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,22 @@ const tweets = require('./modules/tweets')
 
 const followships = require('./modules/followships')
 const userController = require('../controllers/user-controller')
+const likeController = require('../controllers/like-controller')
 
 const { authenticatedUser } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
-
-router.use('/admin', admin)
 router.use('/tweets', authenticatedUser, tweets)
+router.use('/admin', admin)
 router.use('/followships', authenticatedUser, followships)
+router.get('/users/:userId', authenticatedUser, userController.getProfile)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', authenticatedUser, userController.logout)
-router.get('/users/:userId', authenticatedUser, userController.getProfile)
+
+router.get('/users/:id/likes', authenticatedUser, likeController.getUserLike)
+
 router.use('/', (req, res) => {
   res.redirect('/tweets')
 })
--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -3,9 +3,15 @@ const router = express.Router()
 
 const replyController = require('../../controllers/reply-controller')
 const tweetController = require('../../controllers/tweet-controller')
+const likeController = require('../../controllers/like-controller')
+
+router.post('/:id/like', likeController.postLike)
+router.post('/:id/unlike', likeController.postUnlike)
+router.get('/', tweetController.getTweets)
+router.post('/', tweetController.postTweet)
 
 router.get('/:tid/replies', replyController.getReply)
 router.post('/:tid/replies', replyController.postReply)
-router.get('/', tweetController.getTweets)
+router.use('/', tweetController.getTweets)
 
 module.exports = router
